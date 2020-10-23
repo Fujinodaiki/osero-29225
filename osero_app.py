cc2e669e01f88e59a72bfff8f77c9936f8a4f6b4
Fujinodaiki <523.snow@gmail.com>
2020-10-24 03:44:37 +0900

    右クリックで置いたオセロを削除できるように変更

--- osero_app.py
+++ osero_app.py
@@ -8,6 +8,7 @@ cursor_y = 0
 mouse_x = 0
 mouse_y = 0
 mouse_c = 0
+mouse_lc = 0
 pl_t = 0
 
 def mouse_move(e):#マウスの現在地
@@ -15,16 +16,20 @@ def mouse_move(e):#マウスの現在地
   mouse_x = e.x
   mouse_y = e.y
 
-def mouse_press(e):#マウスクリック
+def mouse_press(e):#マウスクリック左クリック
   global mouse_c
   mouse_c = 1
+
+def mouse_right(e):
+  global mouse_lc
+  mouse_lc = 1
 
 osero = [
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
-  [0,0,0,2,1,0,0,0],
-  [0,0,0,1,2,0,0,0],
+  [0,0,0,0,0,0,0,0],
+  [0,0,0,0,2,0,0,0],
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0],
   [0,0,0,0,0,0,0,0]
@@ -38,20 +43,32 @@ def put_osero():#オセロの表示
       elif osero[y][x] == 2:
         cvs.create_image(x*72+36,y*72+36,image=white,tag="OSERO")
 
+      
+
+
 
 def game_main():
-  global cursor_x,cursor_y,mouse_c,pl_t
+  global cursor_x,cursor_y,mouse_c,mouse_lc,pl_t
   if mouse_x < 72*8 and mouse_y < 72*8:
+    fnt = ("Times New Roman",30)
+    txt = "mouse({},{}){}{}".format(mouse_x,mouse_y,mouse_c,mouse_lc)
+    txt1 = "mouse({},{})".format(cursor_x,cursor_y)
+    cvs.delete("TEST")
+    cvs.create_text(250,288, text=txt,fill="black",font=fnt,tag="TEST")
+    cvs.create_text(200,318, text=txt1,fill="black",font=fnt,tag="TEST")
     cursor_x = int(mouse_x/72)
     cursor_y = int(mouse_y/72)
-    if mouse_c == 1:
+    if mouse_c == 1 and mouse_lc == 0:
       mouse_c = 0
-      if pl_t == 0:
-        osero[cursor_y][cursor_x] = 1
-        pl_t = 1
+      if pl_t == 0:     
+            osero[cursor_y][cursor_x] = 1
+          pl_t = 1
       else:
         osero[cursor_y][cursor_x] = 2
         pl_t = 0
+    elif mouse_c == 0 and mouse_lc == 1:
+      mouse_lc = 0
+      osero[cursor_y][cursor_x] = 0
   
   cvs.delete("OSERO")
   put_osero()
@@ -62,7 +79,8 @@ root = tkinter.Tk()
 root.title("普通のオセロ")
 root.resizable(False,False)
 root.bind("<Motion>",mouse_move)
-root.bind("<ButtonPress>",mouse_press)
+root.bind("<Button-1>",mouse_press)
+root.bind("<Button-2>",mouse_right)
 cvs = tkinter.Canvas(root,width=576,height=576)
 cvs.pack()
 
